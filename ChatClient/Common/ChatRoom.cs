0c476598cdcd67c9e29c2d54e8bd7bacb24277d4
Riccardo Treso <riccardo.treso@atm.it>
2022-04-02 16:23:32 +0200

    parse command and response

---
+++ ChatClient/Common/ChatRoom.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using ChatClient.Model;
+
+namespace ChatClient
+{
+    public class ChatRoom
+    {
+        Socket senderClient;
+        public ChatRoom()
+        {
+        }
+
+        public void Connect()
+        {
+
+
+            // Connect to a remote device.  
+            try
+            {
+                // Establish the remote endpoint for the socket.  
+                // This example uses port 11000 on the local computer.  
+                IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+                // Create a TCP/IP  socket.  
+                senderClient = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.  
+                try
+                {
+                    senderClient.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        senderClient.RemoteEndPoint.ToString());
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public bool EnterInRoom(string NickName)
+        {
+            // Data buffer for incoming data.  
+            byte[] bytes = new byte[1024];
+
+            // Encode the data string into a byte array.  
+            byte[] msg = Encoding.ASCII.GetBytes(ChatCommandFactory.Login(NickName).ToString());
+
+            // Send the data through the socket.  
+            int bytesSent = senderClient.Send(msg);
+
+            // Receive the response from the remote device.  
+            int bytesRec = senderClient.Receive(bytes);
+            string responseMessage = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+            var response = ChatResponse.Parse(responseMessage);
+            return !response.IsError;
+            
+        }
+
+        ~ChatRoom() 
+        {
+            // Release the socket.  
+            senderClient.Shutdown(SocketShutdown.Both);
+            senderClient.Close();
+
+        }
+    }
+}
--- ChatClient/Model/ChatCommand.cs
+++ ChatClient/Model/ChatCommand.cs
@@ -3,16 +3,62 @@ using System.Text.Json;
 
 namespace ChatClient.Model
 {
+    public abstract class ChatBase
+    {
+        protected static string EOF = "<EOF>";
+
+        protected static string ClearFromEOF(string message)
+            => message?.Replace(EOF, string.Empty);
+    }
+    public enum Command
+    {
+        LOGIN,
+        LOGOUT,
+        TEXT,
+        COUNT,
+        ALL_MESSAGE
+    }
+
 
     public static class ChatCommandFactory {
         public static ChatCommand Login(string NickName)
-            => new ChatCommand("LOGIN") { Identity = new User(NickName) };
+            => new ChatCommand(Command.LOGIN) { Identity = new User(NickName) };
+
+        public static ChatCommand Text(User Identity, string Text)
+            => new ChatCommand(Command.TEXT) { Identity = Identity, Data = Text };
+
+        public static ChatCommand GetCount()
+            => new ChatCommand(Command.COUNT);
+
+        public static ChatCommand GetAllMessage()
+            => new ChatCommand(Command.ALL_MESSAGE);
+
+        public static ChatCommand Louout()
+            => new ChatCommand(Command.LOGOUT);
     }
 
-    public class ChatCommand
+    public class ChatResponse : ChatBase {
+        public static ChatResponse Parse(string response)
+        {
+            try
+            {
+                return JsonSerializer.Deserialize<ChatResponse>(ClearFromEOF(response));
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("Unable to parse response: " + response);
+            }
+        }
+
+
+        public string Code { get; set; }
+        public bool IsError { get; set; }
+        public string Data { get; set; }
+    }
+
+    public class ChatCommand : ChatBase
     {
-        private string EOF = "<EOF>";
-        public string Command { get; private set; }
+        public Command Command { get; private set; }
         public string Data { get; set; }
         public User Identity { get; set; }
 
@@ -21,7 +67,7 @@ namespace ChatClient.Model
 
         }
 
-        public ChatCommand(string command)
+        public ChatCommand(Command command)
         {
             Command = command;
         }
